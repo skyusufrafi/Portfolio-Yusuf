25f24854902972b806ea6d363a25ecd3b0d8f3ef
kaifmohammed211 <kaifmohammed211@gmail.com>
2026-04-05 22:55:51 +0530

    final portfolio ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ export default function Navbar() {
         <a href="#about" className="hover:text-white">About</a>
         <a href="#skills" className="hover:text-white">Skills</a>
         <a href="#projects" className="hover:text-white">Projects</a>
+        <a href="#contact" className="hover:text-white">Contact</a>
       </div>
     </nav>
   );
